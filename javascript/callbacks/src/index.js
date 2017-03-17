a09436cee202682d3f19bd33bb431bd6c13dcf36
Mark Hayden <hi@markhayden.me>
2017-03-17 15:10:45 -0700

    add render for callback handler and dfp

---
+++ javascript/callbacks/src/index.js
@@ -0,0 +1,20 @@
+(function(d, w) {
+    // add lytics callback
+    w.liosetup = w.liosetup || {};
+    w.liosetup.callback = w.liosetup.callback || [];
+    w.liosetup.addCallback = function(fn){
+        // handle legacy function callback cleanup
+        if(typeof w.liosetup.callback === 'function'){
+            var arr = [];
+            arr.push(w.liosetup.callback);
+            w.liosetup.callback = arr;
+        }
+
+        // check if lytics is loaded
+        if(w.lio && w.lio.loaded){
+            fn(w.lio.data);
+        }else{
+            w.liosetup.callback.push(fn);
+        }
+    }
+}(document, window));
